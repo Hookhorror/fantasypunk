d11b484cc2be881184f627376d135ba551ea6a16
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-01-29 10:56:19 +0200

    Fixing repo

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Spawns the hit effect when bullet collides
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        // Destroys the effect after given time
+        Destroy(effect, 1);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/KillCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillCharacter : MonoBehaviour
+{
+    public GameObject character;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(character);
+    }
+}
