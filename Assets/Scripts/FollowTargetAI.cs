7899b066192d29de8575c510f3edd25eb8d09617
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-02-19 09:44:59 +0200

    Enemy now starts to follow after player step over a given treshold

--- Assets/Scripts/FollowTargetAI.cs
+++ Assets/Scripts/FollowTargetAI.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patrol : MonoBehaviour
+public class FollowTargetAI : MonoBehaviour
 {
     public float speed;
     public Transform[] checkpoints;
     public Rigidbody2D target;
+    public float range;
 
     private int randomCheckpoint;
+
+    void Awake()
+    {
+        // Gizmos.DrawWireSphere(new Vector3(0, 0, 0), 2);
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +26,20 @@ public class Patrol : MonoBehaviour
         // transform.position = Vector2.MoveTowards(transform.position
         //                    , checkpoints[randomCheckpoint].position
         //                    , speed * Time.deltaTime);
-        transform.position = Vector2.MoveTowards(transform.position
-                           , target.position
-                           , speed * Time.deltaTime);
+        float distanceToTarget = Vector2.Distance(transform.position
+                                                , target.transform.position);
+
+        if (distanceToTarget <= range)
+        {
+            transform.position = Vector2.MoveTowards(transform.position
+                              , target.position
+                              , speed * Time.deltaTime);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
