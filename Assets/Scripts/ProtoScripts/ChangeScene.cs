26c7647a66d4e8cf39be56cdd6599cd07329b65c
Niko Sihvo <nikobranart@gmail.com>
2020-02-24 00:13:40 +0200

    SceneChanger prototype...
    Hox... Doors must be game objects not tile objects

---
+++ Assets/Scripts/ProtoScripts/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeScene : MonoBehaviour {
+
+    public string lvlName;
+    
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            SceneManager.LoadScene(lvlName);
+        }
+    }
+}
+ 
+
+
