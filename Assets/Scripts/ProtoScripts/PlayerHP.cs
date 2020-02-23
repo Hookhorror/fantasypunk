3315f2263f606dc5d8573dd41feec573a4e61e72
Niko Sihvo <nikobranart@gmail.com>
2020-02-23 23:26:30 +0200

    HP bar and script to bar and player

---
+++ Assets/Scripts/ProtoScripts/PlayerHP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+
+    public int maxHP = 10;
+    public int currentHP;
+
+    public HPBar hpBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+        hpBar.SetMaxHP(maxHP);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDMG(1);
+        }
+    }
+
+    void TakeDMG(int dmg)
+    {
+        currentHP -= dmg;
+        hpBar.SetHP(currentHP);
+    }
+}
