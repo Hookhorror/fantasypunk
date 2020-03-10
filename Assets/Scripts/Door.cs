9c9b95b7347eb48f944414e5d1d7aa4207a20f62
Niko Sihvo <nikobranart@gmail.com>
2020-03-10 12:56:58 +0200

    Door script and better camMovement

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Vector2 camChange;
+
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
+    private void Start() 
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    private void Update() 
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            cam.minP += camChange;
+            cam.maxP += camChange;
+            other.transform.position += playerChange;
+        }
+    }
+}
