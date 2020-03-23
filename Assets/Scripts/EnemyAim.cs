e559f6c992bb5ba20a22ff681ecbe6cb1d3f6790
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-03-23 15:48:10 +0200

    Player can now aim without rotating goofily

--- Assets/Scripts/EnemyAim.cs
+++ Assets/Scripts/EnemyAim.cs
@@ -7,18 +7,20 @@ public class EnemyAim : MonoBehaviour
     // TODO: Pubblegum solution, same target needs to be set now twice. Super stupid, fix!
     public Rigidbody2D target;
 
+    private float angle;
+    private Vector2 lookDir;
+
     // Update is called once per frame
     void Update()
     {
-
-
-
+        // Updates the looking direction and counts the angle of aiming
+        Vector2 lookDir = target.transform.position - transform.position;
+        angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
     }
 
     void FixedUpdate()
     {
-        Vector2 lookDir = target.transform.position - transform.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        // Turns the aim
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
         // transform.RotateAround(transform.position, new Vector3(0, 0, 1), 1);
---
+++ Assets/Scripts/Player/Aim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    public Camera cam;
+    private Vector2 mousePos;
+    private float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg + 90f;
+    }
+
+    void FixedUpdate()
+    {
+        // Turns the aim
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+}
