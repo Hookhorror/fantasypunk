890e2205cdb0b2827b6125fcabb2109948f1fac1
Brandon <nikobranart@gmail.com>
2020-04-18 17:58:16 +0300

    Tilemap fixing, barrels and pots works problerly

--- Assets/Scripts/barrel.cs
+++ Assets/Scripts/barrel.cs
@@ -1,31 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class barrel : MonoBehaviour
 {
 
     private Animator animator;
 
-    public AudioClip soundEffect;
+    private BoxCollider2D boxCollider;
+
+    public AudioClip[] soundEffect;
     private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    
+    private void Awake() {
+        source = GetComponent<AudioSource>();
     }
 
     public void Smash()
     {
-        animator.SetBool("smash", true);
-        source.PlayOneShot(soundEffect);
-        StartCoroutine(breakCo());
+        try
+        {
+            animator.SetBool("smash", true);
+            source.PlayOneShot(soundEffect[UnityEngine.Random.Range(0, soundEffect.Length)]);
+            boxCollider.enabled = false;
+            StartCoroutine(breakCo());
+        }
+        catch(NullReferenceException ex)
+        {
+            Debug.Log("animator not set");  
+            Debug.Log(ex);  
+        }
+        
     }
 
     public IEnumerator breakCo()
