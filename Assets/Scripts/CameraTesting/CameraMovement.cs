290f48e332eba00b3bbca2fc7b272bdff7dcf025
pritoranta <p.ritoranta@gmail.com>
2020-04-19 14:07:15 +0300

    trying to commit script updates again

---
+++ Assets/Scripts/CameraTesting/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxP;
+    public Vector2 minP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minP.x, maxP.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minP.y, maxP.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+    
+}
