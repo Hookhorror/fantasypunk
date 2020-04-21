a1cd2ced0719473184cceb5a245b7c5abf693e03
Brandon <nikobranart@gmail.com>
2020-04-21 04:12:15 +0300

    Menu

---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public Canvas canvas;
+
+    void Update()
+     {
+         if (Input.GetKeyDown(KeyCode.Escape))
+         {
+              canvas.enabled = true;
+         }
+     }
+
+    
+}
