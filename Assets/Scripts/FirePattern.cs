c48a9f8fa396637c1f23a0d36aad2b298b2b1cbf
pritoranta <p.ritoranta@gmail.com>
2020-05-14 01:56:49 +0300

    magic pot!

--- Assets/Scripts/FirePattern.cs
+++ Assets/Scripts/FirePattern.cs
@@ -6,6 +6,25 @@ public class FirePattern : MonoBehaviour
 {
     public float[] angles;
 
+    // combines existing angles with new angles
+    public float[] combine(float[] newangles)
+    {
+        float[] result = new float[(angles.Length*newangles.Length)];
+        int i = 0;
+        foreach(float f1 in angles)
+        {
+            foreach(float f2 in newangles)
+            {
+                Debug.Log(f1 + ", " + f2);
+                result[i] = f1 + f2;
+                Debug.Log(result[i]);
+                i++;
+            }
+        }
+        angles = result;
+        return result;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
