afdef2076768ef4745c678660cee7cfb6df6de26
joni <joni.kiviluoto@gmail.com>
2020-05-14 16:45:07 +0300

    Added explosion sound to boss death explosion

---
+++ Assets/ToonExplosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToonExplosion : MonoBehaviour
+{
+    // private AudioClip explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // explosion = GetComponent<audiomana
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
